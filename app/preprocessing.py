cca902fd75cad3309a71e20ec9c7dacb2c19fc34
Mufti129 <ukhtytrader01@gmail.com>
2026-04-14 20:29:00 +0700

    preprocessing.py

--- app/preprocessing.py
+++ app/preprocessing.py
@@ -6,11 +6,9 @@ def load_data(path):
 def preprocess(df):
     df = df.copy()
 
-    # Feature Engineering
     df['ltv'] = df['loan_amount'] / df['collateral_value']
     df['risk_flag'] = df['days_late'].apply(lambda x: 1 if x > 7 else 0)
 
-    # Encoding
     df = pd.get_dummies(df, columns=['branch', 'job_type'], drop_first=True)
 
     return df
